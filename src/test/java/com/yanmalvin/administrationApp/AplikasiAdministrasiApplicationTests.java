68c27859ed1cdaaec663174486a4097d20df706c
@malvinpahlevi <malvinfrog@gmail.com>
2020-11-24 14:10:45 +0700

    First Commit

---
+++ src/test/java/com/yanmalvin/administrationApp/AplikasiAdministrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanmalvin.administrationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiAdministrasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
